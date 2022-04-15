db2b8be15a8ac1ba624f84ed2075502483053315
Yuan Cheng <79687050+YuanChengRua@users.noreply.github.com>
2022-04-15 14:50:08 -0400

    Add files via upload

---
+++ secrets1.py
@@ -0,0 +1,1 @@
+api_key = '9SCWSfPhGn6gVYDCP9php0mlHokWRAZO'
--- top_headlines.py
+++ top_headlines.py
@@ -1,1 +1,29 @@
+from flask import Flask, render_template, request
+from secrets1 import *
+import requests
 
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return '<h1> Welcome!</h1>'
+
+
+@app.route('/name/<name>')
+def name(name):
+    return render_template('name.html', name=name)
+
+@app.route('/headlines/<name>')
+def read_artical(name):
+    base_url = 'https://api.nytimes.com/svc/topstories/v2/technology.json?api-key=' + api_key
+    results = requests.get(base_url).json()
+    results = results['results']
+    headlines = [ele['title'] for ele in results[:5]]
+    return render_template('read_artical.html', name=name, headlines=headlines)
+
+
+if __name__ == '__main__':
+
+    print('starting Flask app', app.name)
+
+    app.run(debug=True)
